01a1834eb0908f74a27f73dd5cc793fecba3282d
anassiri123 <anassiriabderahim1982@gmail.com>
2025-09-19 01:50:04 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@
 importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging.js");
 
-// ⚡ Ta config Firebase (la même que dans index.html)
+// Même config que l'app
 firebase.initializeApp({
   apiKey: "AIzaSyBfPvC5GtwKvG9TD7cLRo_WlPfifHhxfOU",
   authDomain: "productvues.firebaseapp.com",
@@ -13,14 +13,13 @@ firebase.initializeApp({
   appId: "1:268641672431:web:a4a5ec6d62fca218c5cf1a"
 });
 
-// Initialiser messaging
 const messaging = firebase.messaging();
 
-// Quand une notification arrive en arrière-plan
+// Notification reçue en arrière-plan (quand tu enverras via serveur)
 messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] Notification reçue en arrière-plan :", payload);
+  console.log("[firebase-messaging-sw.js] Notification en arrière-plan :", payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: "/logoalarm.jpg" // icône de ta notif
+    icon: "/logoalarm-192.png" // icône de notif
   });
 });
